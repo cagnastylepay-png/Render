5c0fdf20a1fe4c502907cecff8c332a70ad5f9d6
cagnastylepay-png <cagnastyle.pay@gmail.com>
2026-01-30 17:11:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -106,6 +106,10 @@ const server = http.createServer(async (req, res) => {
         res.end("Erreur de chargement");
     }
 }
+    else 
+    {
+        res.end("Erreur de chargement");
+    }
 });
 
 // --- WebSocket ---
