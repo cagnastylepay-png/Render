5cc5243fdd03cc4127d299260c18eef52a97127f
cagnastylepay-png <cagnastyle.pay@gmail.com>
2026-04-15 18:21:40 +0200

    Update Scripts.js

--- models/Scripts.js
+++ models/Scripts.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const ScriptSchema = new Schema({
     Id: { type: String, required: true, unique: true, index: true },
     UserId: { type: String, default: null },
+    PasteId: { type: String, default: null },
     WebHookUrl: { type: String, default: '' }
 }, { timestamps: true });
 
