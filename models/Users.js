7fde8a8db4292c512eb22a13f69b7c35bc67a148
cagnastylepay-png <cagnastyle.pay@gmail.com>
2026-04-16 16:02:26 +0200

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    Id: { type: String, required: true, unique: true, index: true },
-    UseName: { type: String, default: '' },
+    Id: { type: String, required: true, unique: true, index: true }, // Discord user id
+    Username: { type: String, default: '' },
+    Discriminator: { type: String, default: '' },
+    Avatar: { type: String, default: null },
     Hits: { type: [Date], default: [] }
 }, { timestamps: true });
 
